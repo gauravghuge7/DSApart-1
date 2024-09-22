387d7e115f63cc96cd16370badfe71d3629b0a26
Gaurav Ghuge <guduughuge7@gmail.com>
2024-09-22 13:22:43 +0530

    update the code

---
+++ StringBuilder/demoBuilder.java
@@ -0,0 +1,29 @@
+
+/**
+ * demoBuilder
+ */
+public class demoBuilder {
+
+
+
+   public static void main(String[] args) {
+      
+
+      StringBuilder st = new StringBuilder();
+
+
+
+      
+      st.append("gaurav");
+      st.append("gaurav");
+
+
+      
+      System.out.println(st);
+      
+
+
+   }
+
+   
+}
